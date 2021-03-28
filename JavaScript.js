71b2abc4ebf75037ec6a88d53d388eb13ca39417
CSimoneCode <csimonecode@gmail.com>
2021-03-28 11:22:23 -0600

    initial commit

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+// function convertTime(x) {
+//   let raw = x 
+//       seconds = parseInt((raw/1000) % 60),
+//       minutes = parseInt((raw/(1000 * 60)) % 60),
+//       hours = parseInt((raw/(1000 * 60 * 60)) % 24);
+//       hours = (hours < 10) ? "0" + hours : hours;
+//       minutes = (minutes < 10) ? "0" + minutes : minutes;
+//       seconds = (seconds < 10) ? "0" + seconds : seconds;
+      
+//       return `Quake occured ${hours} hours ${minutes} minutes ${seconds} seconds ago`;
+//  }
+
+
+// console.log(convertTime(1605908357680))
+  
